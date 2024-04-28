ca1fd0d41132805d1b22306b30b88255fec70fb7
codenamecriz <codenamecriz@gmail.com>
2024-04-29 06:13:56 +0800

    Added Dynamic Property Cards

--- src/app/properties/page.jsx
+++ src/app/properties/page.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Link from "next/link";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/Properties/PropertyCard";
 
 const PropertiesPage = () => {
   return (
-    <div>
-      PropertiesPage
-      <h1 className="text-3xl">Hello!</h1>
-      <Link href="/">Go Home</Link>
-    </div>
+    // <!-- All Listings -->
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+        {properties.lenght === 0 ? (
+          <p>No properties found!</p>
+        ) : (
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
