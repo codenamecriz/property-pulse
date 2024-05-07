53092e5ba0c0b412856b95aaae0210f254f5912e
codenamecriz <codenamecriz@gmail.com>
2024-05-08 06:08:38 +0800

    Added nofound and loading page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,14 @@
-"use client";
 import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
-import { useRouter, useParams } from "next/navigation";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
--- src/app/properties/page.jsx
+++ src/app/properties/page.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import Link from "next/link";
 import properties from "@/properties.json";
 import PropertyCard from "@/components/Properties/PropertyCard";
 
 const PropertiesPage = () => {
   return (
     // <!-- All Listings -->
-    <section class="px-4 py-6">
-      <div class="container-xl lg:container m-auto px-4 py-6">
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
         {properties.lenght === 0 ? (
           <p>No properties found!</p>
         ) : (
-          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {properties.map((property) => (
               <PropertyCard key={property._id} property={property} />
             ))}
