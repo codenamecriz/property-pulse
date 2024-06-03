b533eb0ce6be094baa1b6f2513487efca1c70d72
codenamecriz <codenamecriz@gmail.com>
2024-06-03 08:32:33 +0800

    Added google login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,14 +20,14 @@ const Navbar = () => {
 
   useEffect(() => {
     const setAuthProviders = async () => {
-      const response = await getProviders();
-      setProviders(response);
+      const res = await getProviders();
+      setProviders(res);
     };
 
     setAuthProviders();
   }, []);
 
-  console.log(providers);
+  console.log("Providers: " + providers);
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -107,10 +107,17 @@ const Navbar = () => {
           {!session && (
             <div className="hidden md:block md:ml-6">
               <div className="flex items-center">
-                <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900/40 hover:text-white rounded-md px-3 py-2">
-                  <FaGoogle className="text-white mr-2" />
-                  <span>Login or Register</span>
-                </button>
+                {providers &&
+                  Object.values(providers).map((provider, index) => (
+                    <button
+                      key={index}
+                      onClick={() => signIn(provider.Id)}
+                      className="flex items-center text-white bg-gray-700 hover:bg-gray-900/40 hover:text-white rounded-md px-3 py-2"
+                    >
+                      <FaGoogle className="text-white mr-2" />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
               </div>
             </div>
           )}
@@ -240,13 +247,18 @@ const Navbar = () => {
               </Link>
             )}
 
-            {!session && (
-              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900/40 hover:text-white rounded-md px-3 py-2 my-4">
-                {/* <i className="fa-brands fa-google mr-2"></i> */}
-                <FaGoogle className="text-white mr-2" />
-                <span>Login or Register</span>
-              </button>
-            )}
+            {!session &&
+              providers &&
+              Object.values(providers).map((provider, index) => (
+                <button
+                  key={index}
+                  onClick={() => signIn(provider.Id)}
+                  className="flex items-center text-white bg-gray-700 hover:bg-gray-900/40 hover:text-white rounded-md px-3 py-2"
+                >
+                  <FaGoogle className="text-white mr-2" />
+                  <span>Login or Register</span>
+                </button>
+              ))}
           </div>
         </div>
       )}
--- src/utils/authOptions.js
+++ src/utils/authOptions.js
@@ -1,4 +1,8 @@
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider from 'next-auth/providers/google';
+
+import connectDB from '@/config/database';
+import User from '@/models/User';
+
 
 export const authOptions = {
     providers: [
@@ -19,15 +23,29 @@ export const authOptions = {
         // Invoked on successful signin
         async signIn({profile}){
             //1. Connect to database
+            await connectDB();
             //2. Check if user exist
+            const userExist = await User.findOne({email: profile.email});
             //3. If not, then add user to database
+            if(!userExist){
+              // Truncate username if too long
+              const username = profile.name.slice(0, 20);
+              await User.create({email: profile.email, 
+                username: username,
+                imate: profile.picture
+              })
+            }
             //4. Return true to allow signin
+            return true;
         },
         // Modify th session object
         async session({session}){
             //1. Get user from database
+            const user =await User.findOne({email: session.user.email});
             //2. Assign the user id to the session
+            session.user.id = user._id.toString();
             //3. Return session
+            return session;
         }
       }
 };
