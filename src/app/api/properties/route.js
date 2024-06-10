a5a2b16dd96ae3042599a33dca9f623ebeda1490
codenamecriz <codenamecriz@gmail.com>
2024-06-11 06:36:05 +0800

    Added cloudinary setup and Upload image functionality

--- src/app/api/properties/route.js
+++ src/app/api/properties/route.js
@@ -1,3 +1,4 @@
+import cloudinary from "@/config/cloudinary";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
@@ -67,6 +68,34 @@ export const POST = async (request) => {
       owner: userId,
       //images,
     };
+
+    // Upload Images to cloudinary
+    const imageUploadPromises = [];
+
+    for (const image of images) {
+      const imageBuffer = await image.arrayBuffer();
+      const imageArray = Array.from(new Uint8Array(imageBuffer));
+      const imageData = Buffer.from(imageArray);
+
+      // Convert the image data to base 64
+      const imageBase64 = imageData.toString("base64");
+
+      // Make request to upload to cloudinary
+      const result = await cloudinary.uploader.upload(
+        `data:image/png;base64,${imageBase64}`,
+        {
+          folder: "propertypulse",
+        }
+      );
+
+      imageUploadPromises.push(result.secure_url);
+
+      // Wait for all images to upload
+      const uploadedImages = await Promise.all(imageUploadPromises);
+
+      // Add uploaded images to propertyData object
+      propertyData.images = uploadedImages;
+    }
 
     console.log(propertyData);
 
