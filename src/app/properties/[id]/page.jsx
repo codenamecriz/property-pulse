d7c7fedd7c46ba35d6a46d0fb0c213d577c647b8
codenamecriz <codenamecriz@gmail.com>
2024-11-28 12:29:06 +0800

    Added PropertyCard and PropertyImages

--- src/app/properties/[id]/page.jsx
+++ src/app/properties/[id]/page.jsx
@@ -6,6 +6,7 @@ import { fetchProperty } from "@/utils/requests";
 import PropertyHeaderImage from "@/components/Properties/PropertyHeaderImage";
 import Link from "next/link";
 import PropertyDetails from "@/components/Properties/PropertyDetails";
+import PropertyImages from "@/components/Properties/PropertyImages";
 import { FaArrowLeft } from "react-icons/fa";
 import Spinner from "@/components/Spinner";
 
@@ -151,6 +152,7 @@ const PropertyPage = () => {
               </div>
             </div>
           </section>
+          <PropertyImages images={property.images} />
         </>
       )}
     </>
--- src/utils/requests.js
+++ src/utils/requests.js
@@ -2,46 +2,44 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetch all property
 async function fetchProperties() {
-    try {
-        if (!apiDomain) {
-            return [];
-        }
-
-        const response = await fetch(
-        `${apiDomain}/properties`
-      );
-  
-      if (!response) {
-        throw new Error("Failed to fetch data.");
-      }
-  
-      return response.json();
-    } catch (error) {
-      console.log(error);
+  try {
+    if (!apiDomain) {
       return [];
     }
+
+    const response = await fetch(`${apiDomain}/properties`, {
+      cache: "no-store",
+    });
+
+    if (!response) {
+      throw new Error("Failed to fetch data.");
+    }
+
+    return response.json();
+  } catch (error) {
+    console.log(error);
+    return [];
   }
+}
 
-  // Fetch single property
-  async function fetchProperty(id) {
-    try {
-        if (!apiDomain) {
-            return null;
-        }
-
-        const response = await fetch(
-        `${apiDomain}/properties/${id}`
-      );
-  
-      if (!response) {
-        throw new Error("Failed to fetch data.");
-      }
-  
-      return response.json();
-    } catch (error) {
-      console.log(error);
+// Fetch single property
+async function fetchProperty(id) {
+  try {
+    if (!apiDomain) {
       return null;
     }
-  }
 
-  export { fetchProperties, fetchProperty };
+    const response = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!response) {
+      throw new Error("Failed to fetch data.");
+    }
+
+    return response.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
